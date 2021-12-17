3b341b61ee794382e782be49872553549b8b5016
mawenxi2112 <mawenxi2112@gmail.com>
2021-12-18 04:16:25 +0800

    working daily progress entry and logger

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
 import pygsheets
-import numpy as np
-import pandas as pd
-from datetime import datetime
+import inquirer
+import logging
+from src.logger import logger, date_formatted
 
-#Authenticate client access to spreadsheet
+# Table Variables (Make into config)
+dailyProgressTableRange = "A16:D60"
+claimProgressTable = "F16:I58"
+
+# Authenticate client access to spreadsheet
 googleConsole = pygsheets.authorize()
 
-#Open spreadsheet and worksheet
+# Open spreadsheet and worksheet
 spreadSheet = googleConsole.open('BCRYPTO')
 workSheet = spreadSheet.sheet1
 
-#Table
-dailyProgressTable = "A16"
-claimProgressTable = "A16"
 
 def add_daily_progress_entry():
 
-    date = datetime.now().strftime("%d/%m/%Y %H:%M")
-    bcoin = input("Enter current bcoin: ")
+    dailyprogresstable = workSheet.range(dailyProgressTableRange)
+    for row in dailyprogresstable:
 
-    workSheet.append_table([date, bcoin], dailyProgressTable, None, 'ROWS', true)
-    print("New Daily Progress Entered for " + date + " with " + str(bcoin) + " bcoin")
+        if not row[0].value:
 
-testList = workSheet.range("A16:D60")
-for i in testList:
-    print(i)
+            date = date_formatted("%d/%m/%Y %I:%M%p")
+            bcoin = input("enter current bcoin in chest: ")
+
+            workSheet.update_value(row[0].label, date)
+            workSheet.update_value(row[1].label, bcoin)
+
+            logger("successfully added new entry for daily progress for " + date + " with " + bcoin + " bcoin", "green")
+            return
+
+    print("failed to find any empty rows to add new entry!")
+
+def add_token_claim_entry():
+    pass
+
+def generate_summary():
+    pass
+
+def dump_to_csv():
+    pass
+
+while 1 > 0:
+
+    choice = inquirer.list_input("select", choices=['add daily progress', 'add token claim', 'generate summary', 'dump to csv', 'quit'])
+
+    if choice == "add daily progress":
+        add_daily_progress_entry()
+    elif choice == "add token claim":
+        add_token_claim_entry()
+    elif choice == "generate summary":
+        generate_summary()
+    elif choice == "dump to csv":
+        dump_to_csv()
+    elif choice == "quit":
+        break
 
---
+++ src/logger.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import sys
+import yaml
+
+stream = open("./config.yaml", 'r')
+cfg = yaml.safe_load(stream)
+
+COLOR = {
+    'blue': '\033[94m',
+    'default': '\033[99m',
+    'grey': '\033[90m',
+    'yellow': '\033[93m',
+    'black': '\033[90m',
+    'cyan': '\033[96m',
+    'green': '\033[92m',
+    'magenta': '\033[95m',
+    'white': '\033[97m',
+    'red': '\033[91m'
+}
+
+
+def date_formatted(format = "%d/%m/%Y %I:%M:%S%p"):
+    return datetime.now().strftime(format)
+
+
+def logger(message, color='default'):
+
+    color_formatted = COLOR.get(color.lower(), COLOR['default'])
+
+    formatted_datetime = date_formatted()
+    formatted_message = "[{}] {}".format(formatted_datetime, message)
+    formatted_message_colored = color_formatted + formatted_message + '\033[0m'
+
+    print(formatted_message_colored + "\n")
+
+    if cfg['save_log_to_file']:
+        logger_file = open("./logs/logger.log", "a", encoding='utf-8')
+        logger_file.write(formatted_message + '\n')
+        logger_file.close()
+
+    return True
